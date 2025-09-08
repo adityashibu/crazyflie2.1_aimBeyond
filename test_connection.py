8346277093cc5780b78b069ec69ceaf60fac7d1a
Aditya S <as2397@hw.ac.uk>
2025-09-08 12:35:39 +0400

    Added test code for take off and hovering

---
+++ test_connection.py
@@ -0,0 +1,44 @@
+import cflib.crtp
+import time
+
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+URI = 'radio://0/80/2M'
+
+cflib.crtp.init_drivers()
+
+with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+    print("Connected to Crazyflie!")
+
+    print("Arming motors...")
+    for _ in range(10):
+        scf.cf.platform.send_arming_request(True)
+        scf.cf.commander.send_setpoint(0, 0, 0, 0)
+        time.sleep(0.05)
+
+    print("Starting propellers (minimum thrust)...")
+    for _ in range(100):
+        scf.cf.commander.send_setpoint(0, 0, 0, 10000)
+        time.sleep(0.01)
+
+    print("Taking off to moderate altitude...")
+    for _ in range(250):
+        scf.cf.commander.send_setpoint(0, 0, 0, 37500)
+        time.sleep(0.01)
+
+    print("Hovering at altitude...")
+    for _ in range(200):
+        scf.cf.commander.send_setpoint(0, 0, 0, 35000)
+        time.sleep(0.01)
+
+    print("Landing...")
+    for thrust in range(35000, 10000, -500):
+        scf.cf.commander.send_setpoint(0, 0, 0, thrust)
+        time.sleep(0.05)
+
+    scf.cf.commander.send_stop_setpoint()
+    print("Propellers stopped.")
+
+    scf.cf.platform.send_arming_request(False)
+    time.sleep(0.5)
